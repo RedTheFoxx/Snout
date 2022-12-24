147ec2adc9432f82a1010b352dcf875125b84124
RedTheFoxx <morisseau.jeremy@gmail.com>
2022-12-24 01:30:27 +0100

    définition des transactions

--- Modules/Banking.cs
+++ Modules/Banking.cs
@@ -53,7 +53,15 @@ public class Transaction
     public TransactionType? Type { get; set; }
     public decimal Amount { get; set; }
     public DateTime Date { get; set; }
+
+    public Transaction(TransactionType type, decimal amount)
+    {
+        Type = type;
+        Amount = amount;
+        Date = DateTime.Now;
+    }
 }
+
 
 public enum TransactionType
 {
