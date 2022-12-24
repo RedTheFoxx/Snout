9bb80f2ff4cd51dd848e580011855b30b49ed6bf
RedTheFoxx <morisseau.jeremy@gmail.com>
2022-12-24 01:26:59 +0100

    suite des définitions de classe

--- Modules/Banking.cs
+++ Modules/Banking.cs
@@ -1,12 +1,15 @@
-class User 
+public class User
 {
+    public int? UserId { get; set; }
+    public string? DiscordId { get; set; }
 
-public int? UserId { get; set; }
-public string? DiscordId { get; set; }
-
+    public User(string discordId)
+    {
+        DiscordId = discordId ?? throw new ArgumentNullException(nameof(discordId));
+    }
 }
 
-class Account
+public class Account
 {
     public string? AccountNumber { get; set; }
     public AccountType? Type { get; set; }
@@ -17,6 +20,31 @@ class Account
     public decimal InterestRate { get; set; }
     public decimal AccountFees { get; set; }
     public List<Transaction>? TransactionHistory { get; set; }
+
+    // CONSTRUCTEUR : (AccountNumber, Type, Currency, AccountHolders) sont obligatoires et ne peuvent pas être null. Si l'un de ces champs est null => 
+    // => une exception ArgumentNullException est levée. Le champ AccountHolders doit également contenir au moins un élément, sinon une exception ArgumentException est levée. 
+    // Les autres champs ont des valeurs par défaut et peuvent être null si elles ne sont pas fournies.
+    public Account(string accountNumber, AccountType type, List<User> accountHolders, decimal balance, string currency, decimal overdraftLimit, decimal interestRate, decimal accountFees, List<Transaction> transactionHistory)
+    {
+        AccountNumber = accountNumber ?? throw new ArgumentNullException(nameof(accountNumber));
+        if (!Enum.IsDefined(typeof(AccountType), type))
+        {
+            throw new ArgumentException("Type de compte non valide", nameof(type));
+        }
+        Type = type == AccountType.None ? AccountType.Checking : type;
+
+        AccountHolders = accountHolders ?? throw new ArgumentNullException(nameof(accountHolders));
+        if (AccountHolders.Count == 0)
+        {
+            throw new ArgumentException("Il doit y avoir au moins un propriétaire du compte", nameof(accountHolders));
+        }
+        Balance = balance;
+        Currency = currency ?? throw new ArgumentNullException(nameof(currency));
+        OverdraftLimit = overdraftLimit;
+        InterestRate = interestRate;
+        AccountFees = accountFees;
+        TransactionHistory = transactionHistory;
+    }
 }
 
 public class Transaction
@@ -27,16 +55,17 @@ public class Transaction
     public DateTime Date { get; set; }
 }
 
-public class TransactionType
+public enum TransactionType
 {
-    public int TransactionTypeId { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
+    Deposit,
+    Withdrawal,
+    Transfer
 }
 
-public class AccountType
+public enum AccountType
 {
-    public int AccountTypeId { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
+    Checking,
+    Savings,
+    Locked,
+    None
 }
