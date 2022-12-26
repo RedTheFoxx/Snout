49517c54749579ec473f95dce8a50e85af670e75
RedTheFoxx <morisseau.jeremy@gmail.com>
2022-12-26 22:00:38 +0100

    ++ modal de nouveau compte

--- Core.cs
+++ Core.cs
@@ -519,11 +519,22 @@ public class Program
 
     private async Task HandleNewAccountCommand(SocketSlashCommand command)
     {
-        // TODO : Ouvrir une modal avec les champs à compléter
+        var modal = new ModalBuilder();
+
+        modal.WithTitle("Créer un nouveau compte")
+            .WithCustomId("new_account_modal")
+            .AddTextInput("Propriétaire", "new_account_userid_textbox", TextInputStyle.Short, placeholder: "0 (ID DB)", required: true)
+            .AddTextInput("Type de compte", "new_account_type_textbox", TextInputStyle.Short, placeholder: "Checkings / Savings / Locked", required: true)
+            .AddTextInput("Limite de découvert", "new_account_overdraft_textbox", TextInputStyle.Short, placeholder: "1000", required: true)
+            .AddTextInput("Taux d'intérêt", "new_account_interest_textbox", TextInputStyle.Short, placeholder: "0.02", required: true)
+            .AddTextInput("Frais de service", "new_account_fees_textbox", TextInputStyle.Short, placeholder: "8", required: true);
+            
+
+        await command.RespondWithModalAsync(modal.Build());
     }
 
 
-/////////// METHODES DIVERSES /////////////
+/////////// FONCTIONS DIVERSES /////////////
 ///////////////////////////////////////////
     public async Task SendEmbedPrivateMessageAsync(DiscordSocketClient client, ulong userId, Embed embed)
     {
