c161dc0f6aefc07ed866a956a82eee8c19d5b573
RedTheFoxx <morisseau.jeremy@gmail.com>
2022-12-27 00:48:01 +0100

    // simplify account class

--- Modules/Banking.cs
+++ Modules/Banking.cs
@@ -2,35 +2,28 @@ using System.Data.SQLite;
 
 public class Account
 {
-    public string? AccountNumber { get; set; }
-    public AccountType? Type { get; set; }
+    public int? AccountNumber { get; set; }
+    public string? Type { get; set; }
     public SnoutUser? AccountHolder { get; set; }
     public decimal Balance { get; set; }
     public string? Currency { get; set; }
     public decimal OverdraftLimit { get; set; }
     public decimal InterestRate { get; set; }
     public decimal AccountFees { get; set; }
-    public List<Transaction>? TransactionHistory { get; set; }
 
     // CONSTRUCTEUR : (AccountNumber, Type, Currency, AccountHolders) sont obligatoires et ne peuvent pas être null. Si l'un de ces champs est null => 
     // => une exception ArgumentNullException est levée. Le champ AccountHolders doit également contenir au moins un élément, sinon une exception ArgumentException est levée. 
     // Les autres champs ont des valeurs par défaut et peuvent être null si elles ne sont pas fournies.
-    public Account(string accountNumber, AccountType type, SnoutUser accountHolder, decimal balance, string currency, decimal overdraftLimit, decimal interestRate, decimal accountFees, List<Transaction> transactionHistory)
+    public Account(int accountNumber, string type, SnoutUser accountHolder, decimal balance, string currency, decimal overdraftLimit, decimal interestRate, decimal accountFees)
     {
-        AccountNumber = accountNumber ?? throw new ArgumentNullException(nameof(accountNumber));
-        if (!Enum.IsDefined(typeof(AccountType), type))
-        {
-            throw new ArgumentException("Type de compte non valide", nameof(type));
-        }
-        Type = type == AccountType.None ? AccountType.Checking : type;
-
+        AccountNumber = accountNumber;
+        Type = type;
         AccountHolder = accountHolder ?? throw new ArgumentNullException(nameof(accountHolder));
         Balance = balance;
         Currency = currency ?? throw new ArgumentNullException(nameof(currency));
         OverdraftLimit = overdraftLimit;
         InterestRate = interestRate;
         AccountFees = accountFees;
-        TransactionHistory = transactionHistory;
     }
 
     public void RegisterAccount()
@@ -89,12 +82,4 @@ public enum TransactionType
     Deposit,
     Withdrawal,
     Transfer
-}
-
-public enum AccountType
-{
-    Checking,
-    Savings,
-    Locked,
-    None
 }
