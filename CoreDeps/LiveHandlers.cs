6e2bd89f659bf4c575fdd05c124586f7ec212ace
RedTheFoxx <morisseau.jeremy@gmail.com>
2023-01-11 12:08:05 +0100

    // db declaration done (invoked actions)

--- CoreDeps/LiveHandlers.cs
+++ CoreDeps/LiveHandlers.cs
@@ -11,20 +11,20 @@ internal class LiveHandlers
      * 
      * Declared actions in database :
      * 
-     * action_TYPING : When a user starts typing in a channel.
+     * - action_TYPING : When a user starts typing in a channel.
      * - action_MESSAGE_SENT : When a message is sent in a channel.
-     * action_MESSAGE_DELETED : When a message is deleted in a channel.
-     * action_MESSAGE_UPDATED : When a message is updated in a channel.
-     * action_REACTION_ADDED : When a reaction is added to a message.
-     * action_REACTION_REMOVED : When a reaction is removed from a message.
-     * action_MODAL_SUBMITTED : When a modal is submitted.
-     * action_SELECT_MENU_EXECUTED : When a select menu is executed.
+     * - action_MESSAGE_DELETED : When a message is deleted in a channel.
+     * - action_MESSAGE_UPDATED : When a message is updated in a channel.
+     * - action_REACTION_ADDED : When a reaction is added to a message.
+     * - action_REACTION_REMOVED : When a reaction is removed from a message.
+     * - action_MODAL_SUBMITTED : When a modal is submitted.
+     * - action_SELECT_MENU_EXECUTED : When a select menu is executed.
      * - action_TAGUED_BY : When a user is tagged by another user.
-     * action_VOICE_CHANNEL_USER_STATUS_UPDATED : When a user's status in a voice channel is updated.
+     * - action_VOICE_CHANNEL_USER_STATUS_UPDATED : When a user's status in a voice channel is updated.
      * - action_CHANGED_STATUS : When a user changes his status. 
      * - action_MESSAGE_SENT_WITH_FILE : When a message is sent with a file in a channel. 
      * - action_TAGUED_SOMEONE : When a user tags someone in a message. 
-     * - action_USED_SNOUT_COMMAND : When a user uses a Snout command.
+     * - action_USED_SNOUT_COMMAND : When a user uses a Snout command. 
      * 
      * Each function is used to handle an event but its scope is not limited to the Paycheck modules, it can be reused for future things.
      * 
